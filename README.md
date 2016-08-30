8b500ca366cadc80f539340820e9e1e4962deadc
Alexsander <561298@gmail.com>
2016-08-30 15:05:30 +0300

    Catch vk api error

--- README.md
+++ README.md
@@ -13,8 +13,19 @@ vkRequest.method('audio.get', { owner_id: USER_ID, count: COUNT, offset: OFFSET 
 .then(function(json) {
 	console.log(json);
 })
+.catch(VK.VkApiError, function(error) {
+	console.log(`VKApi error ${error.error_code} ${error.error_msg}`);
+	switch(error.error_code) {
+		case 14:
+			console.log('Captcha error');
+		break;	
+		case 5:
+			console.log('No auth');
+		break;
+	}
+})
 .catch(function(error) {
-	console.log(error);
+	console.log(`Other error ${error}`);
 });
 
 //Reqeust to VK api for get access token by user code
--- index.js
+++ index.js
@@ -1,28 +1,5 @@
 module.exports = {
 	VkRequest: require('./lib/vkRequest'),
-	VkToken: require('./lib/vkToken')
+	VkToken: require('./lib/vkToken'),
+	VkApiError: require('./lib/vkApiError')
 }
-
-/*
-var a = require('./lib/vkRequest');
-var b = require('./lib/vkToken');
-
-var d = new b('5484003', 'h3eFqpC0IFZwo3uS6NAa');
-d.getTokenFromCode('789e12c040d18b31a8')
-.then(function(json) {
-	console.log(json);
-})
-.catch(function(error) {
-	console.log(error);
-});
-
-var c = new a('b9edf7a22aff282e050705b5dabf6d0f3446812be76c3aa1a50c66607d6f1510e596871c88da20716b831');
-
-c.method('audio.search', {count: 1})
-.then(function(json) {
-	//console.log(json);
-})
-.catch(function(error) {
-	console.log(error);
-});
-*/
